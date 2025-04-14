0d59e0353ca8d0d9332093fb4a8d8216f0314211
Saikiranreddy Peddavootla <saikiranreddypeddavootla@Saikiranreddys-MacBook-Air.local>
2025-04-14 18:23:17 -0500

    Initial commit

---
+++ app/controllers/admin/base_controller.rb
@@ -0,0 +1,14 @@
+# app/controllers/admin/base_controller.rb
+class Admin::BaseController < ApplicationController
+    before_action :authenticate_user!
+    before_action :require_admin!
+  
+    private
+  
+    def require_admin!
+      unless current_user&.role == "admin"
+        redirect_to root_path, alert: "You are not authorized to access admin area."
+      end
+    end
+end
+  
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,29 @@
+class UsersController < ApplicationController
+  before_action :authenticate_user!
+  
+  def show
+    @user = current_user
+  end
+
+  def edit
+    @user = current_user
+  end
+
+  def update
+    @user = current_user
+    if @user.update(user_params)
+      redirect_to user_path(@user), notice: 'Profile updated successfully!' 
+    else
+      render :edit 
+    end
+  end
+
+  private
+
+  # Strong parameters for user profile update
+  def user_params
+    params.require(:user).permit(:email, :first_name, :last_name)
+  end
+
+
+end
